62a80acfb34707270614dd357d96e19c5a289c0c
Kateřina Baňacká <banackakaterina@gmail.com>
2022-04-30 11:40:47 +0200

    Zaktivnění hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 
 console.log('funguju!');
+
+const hamburger = document.querySelector('#nav-btn');
+const menu = document.querySelectorAll('a');
+
+const closeMenu = () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+};
+
+hamburger.addEventListener('click', closeMenu);
+
+for (let i = 0; i < menu.length; i++) {
+  menu[i].addEventListener('click', closeMenu);
+}
